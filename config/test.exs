438cf8c6403886959c16bf0ff57614dfa92d1eb8
Piyush thapa <piyush.codelabs@gmail.com>
2022-10-17 00:52:38 +0545

    getStream configuration

---
+++ config/test.exs
@@ -0,0 +1,8 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+use Mix.Config
+
+config :getstream,
+  region: "test_region",
+  key: "test_key",
+  secret: "test_secret"
