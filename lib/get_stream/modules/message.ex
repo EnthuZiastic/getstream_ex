b0ded61db6ead57c149793123abac79fe73d48d0
KhemPoudel <me.khempoudel@gmail.com>
2022-11-03 16:03:04 +1100

    add function to upload file

--- lib/get_stream/modules/message.ex
+++ lib/get_stream/modules/message.ex
@@ -21,9 +21,24 @@ defmodule GetStream.Modules.Message do
     create_message(channel_type, channel_id, params)
   end
 
+  def upload_file(channel_type, channel_id, params) do
+    Request.new()
+    |> Request.with_path("channels/#{channel_type}/#{channel_id}/file")
+    |> Request.with_token()
+    |> Request.with_method(:post)
+    |> Request.with_body(params)
+    |> Request.send()
+    |> parse_resp()
+  end
+
   defp parse_resp({:ok, %{"message" => message} = resp}) do
     IO.inspect(resp: resp)
     {:ok, message}
+  end
+
+  defp parse_resp({:ok, %{"file" => file} = resp}) do
+    IO.inspect(resp: resp)
+    {:ok, file}
   end
 
   defp parse_resp(resp), do: resp
