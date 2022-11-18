83e342b65e180d49ca6ad3ac69e5054a08fe740b
KhemPoudel <me.khempoudel@gmail.com>
2022-11-18 12:44:29 +1100

    allow opts in create message

--- lib/get_stream/modules/message.ex
+++ lib/get_stream/modules/message.ex
@@ -4,8 +4,13 @@ defmodule GetStream.Modules.Message do
   """
   alias GetStream.Request
 
-  def create_message(channel_type, channel_id, params) do
-    params = %{"message" => params}
+  @type opts() :: [skip_push: boolean()]
+
+  @spec create_message(String.t(), String.t(), map, __MODULE__.opts()) ::
+          {:error, any} | {:ok, any}
+  def create_message(channel_type, channel_id, message_params, opts \\ []) do
+    skip_push = Keyword.get(opts, :skip_push, false)
+    params = %{"message" => message_params, "skip_push" => skip_push}
 
     Request.new()
     |> Request.with_path("channels/#{channel_type}/#{channel_id}/message")
@@ -16,11 +21,7 @@ defmodule GetStream.Modules.Message do
     |> parse_resp()
   end
 
-  def create_message(channel_type, channel_id, user_id, content) do
-    params = %{"text" => content, "user_id" => user_id}
-    create_message(channel_type, channel_id, params)
-  end
-
+  @spec upload_file(any, any, any) :: {:error, any} | {:ok, any}
   def upload_file(channel_type, channel_id, params) do
     Request.new()
     |> Request.with_path("channels/#{channel_type}/#{channel_id}/file")
@@ -31,13 +32,11 @@ defmodule GetStream.Modules.Message do
     |> parse_resp()
   end
 
-  defp parse_resp({:ok, %{"message" => message} = resp}) do
-    IO.inspect(resp: resp)
+  defp parse_resp({:ok, %{"message" => message}}) do
     {:ok, message}
   end
 
-  defp parse_resp({:ok, %{"file" => file} = resp}) do
-    IO.inspect(resp: resp)
+  defp parse_resp({:ok, %{"file" => file}}) do
     {:ok, file}
   end
 
